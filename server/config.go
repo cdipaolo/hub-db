e34b10457eff22ba1f2a846a2c591f4cb1dd9b80
Conner DiPaolo <cdipaolo96@gmail.com>
2015-12-11 13:27:41 -0800

    Added config file handling

---
+++ server/config.go
@@ -0,0 +1,61 @@
+package server
+
+import ()
+
+const (
+	configPath string = "./config.json"
+)
+
+var (
+	// Config stores the configuration
+	// variable
+	Config *Configuration
+
+	// config holds the configuration
+	// path
+	config string
+)
+
+// Configuration holds config information
+// necessary to run the PornHub crawler
+//
+// TimeDelaySeconds is the number of seconds
+// to wait between crawling each search page
+// (between each ~350 image batches)
+type Configuration struct {
+	Port       int16 `json:"port,omitempty"`
+	portString string
+
+	TimeDelaySeconds uint64 `json:"time_delay,omitempty"`
+}
+
+// init takes in the config path from
+// the command line flags
+func init() {
+	flag.StringVar(&config, "C", configPath, "Sets the server configuration filepath")
+}
+
+// ParseConfigFromFile takes the passed
+// config file path and parses the config
+// params from the file located there
+func ParseConfigFromFile() error {
+	path, err := filepath.Abs(config)
+	if err != nil {
+		return fmt.Errorf("ERROR: error generating absolute filepath from the given config path. Does this file exist? %v", err)
+	}
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("ERROR: error reading the bytes from the configuration filename. %v", err)
+	}
+
+	err = json.Unmarshal(bytes, &Config)
+	if err != nil {
+		return fmt.Errorf("ERROR: error unmarshalling config file from JSON to struct. Is it in json format? %v", err)
+	}
+
+	if Config.Port == 0 {
+		Config.Port = 8080
+	}
+	Config.portString = fmt.Sprintf(":%v", Config.Port)
+}
