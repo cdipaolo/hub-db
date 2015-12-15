c04a139e44623be897c911c505c12eab18f2fa27
Conner DiPaolo <cdipaolo96@gmail.com>
2015-12-15 00:53:12 -0800

    Added MRJob MapReduce job to convert raw data to album dataset and convert data

--- data/process/albums.py
+++ data/process/albums.py
@@ -1,11 +1,33 @@
+import json
+import os.path
+import re
+
+import mrjob.protocol
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import json
+
+IMAGE_ID_RE = re.compile(r"[0-9]+")
+
 
 class MRDeriveAlbumDataset(MRJob):
+    OUTPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
 
     def mapper(self, _, line):
         data = json.loads(line)
-        page = data["page"]
+        ## yield each album after processing
         for album in data["albums"]:
-            yield page, album
+            album["album_id"] = os.path.basename(album["URI"])
+
+            ## only include image id's and not full info
+            images = []
+            for image in album["images"]:
+                images.append(int(IMAGE_ID_RE.findall(os.path.basename(image["uri"]))[0]))
+            album["images"] = images
+            yield album["album_id"], album
+
+    def reducer(self, _, album):
+        for val in album:
+            yield None, val
+
+if __name__ == '__main__':
+    MRDeriveAlbumDataset.run()
